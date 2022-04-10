ab541e4ce9251cf8bb4df31b7794ecfcc7c9c294
ischo <chois05@whatap.io>
2022-04-10 12:32:26 +0900

    feat(index): graphQL 연동

--- src/index.js
+++ src/index.js
@@ -1,7 +1,60 @@
-const express  = require('express')
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!!'))
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Arrison' }
+];
+
+// 그래프QL 스키마 언어로 스키마를 구성
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+// 스키마 필드를 위한 리졸버 함수 제공
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World',
+    notes: () => notes,
+    note: (parent, args) => notes.find(note => note.id === args.id)
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const port = process.env.PORT || 4000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+app.listen({ port }, () => {
+  console.log(
+    `GraphQL Server Running at http://localhost:${port}${server.graphqlPath}`
+  );
+});
