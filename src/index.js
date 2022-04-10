8e0e89bc609a6c9c3625c917a3aec9af4fc0c8fc
ischo <chois05@whatap.io>
2022-04-10 13:11:50 +0900

    feat(mongo): mongoose 연동

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
 
-const app = express();
+const db = require('./db');
+const models = require('./models');
 
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Arrison' }
-];
+// .env 파일에 명시된 포트 또는 포트 4000에서 서버를 실행
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
 
 // 그래프QL 스키마 언어로 스키마를 구성
 const typeDefs = gql`
@@ -32,27 +33,30 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
-    notes: () => notes,
-    note: (parent, args) => notes.find(note => note.id === args.id)
+    notes: async () => await models.Note.find(),
+    note: async (parent, args) => await models.Note.findById(args.id)
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: 'Adam Scott'
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     }
   }
 };
 
-const port = process.env.PORT || 4000;
+const app = express();
 
+// DB에 연결
+db.connect(DB_HOST);
+
+// 아폴로 서버 설정
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// 아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
 server.applyMiddleware({ app, path: '/api' });
+
 app.listen({ port }, () => {
   console.log(
     `GraphQL Server Running at http://localhost:${port}${server.graphqlPath}`
